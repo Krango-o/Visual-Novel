2b1713dae5d2d11a35aac55b6982e9c3a18c6e82
Peter Lockhart <peterlockhart72@gmail.com>
2022-10-26 22:27:48 -0400

    novel engine

---
+++ My project/Assets/NovelEngine/Managers/EventManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    public UnityEvent TransitionMidMovement = new UnityEvent();
+    public UnityEvent Pause = new UnityEvent();
+    public UnityEvent Unpause = new UnityEvent();
+}
---
+++ My project/Assets/NovelEngine/Managers/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    
+    public EventManager EventManager { get; private set; }
+    public UIUtility UIUtility { get; private set; }
+    public SaveManager SaveManager { get; private set; }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            //Starting up the game
+            instance = this;
+            EventManager = this.GetComponent<EventManager>();
+            UIUtility = this.GetComponent<UIUtility>();
+            SaveManager = this.GetComponent<SaveManager>();
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
