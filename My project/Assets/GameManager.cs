31a3793858d9f2fdc3a105851361a8154595a631
Peter Lockhart <peterlockhart72@gmail.com>
2022-11-01 14:53:49 -0400

    Fix game manager bug

--- My project/Assets/GameManager.cs
+++ My project/Assets/GameManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
 
 public class GameManager : MonoBehaviour
 {
